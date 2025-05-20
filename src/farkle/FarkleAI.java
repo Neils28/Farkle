4e68ce5f05fa235bb19d6be62fd13f918e27f651
djcarlson2 <djcarlson2@students.unwsp.edu>
2025-05-20 11:48:54 -0500

    AI scoring works now

--- src/farkle/FarkleAI.java
+++ src/farkle/FarkleAI.java
@@ -18,13 +18,6 @@ public class FarkleAI {
     // Main method AI should call when it's its turn (Player 2)
     public void takeTurn() {
 
-        if (model.getCurrentPlayer() != 1 || model.isGameOver()) {
-            // show popup message
-            JOptionPane.showMessageDialog(view, "It's not AI's turn or the game is over.", "Invalid Turn",
-                    JOptionPane.INFORMATION_MESSAGE);
-            return;
-        }
-
         int rolls = 0;
 
         while (rolls < 3) {
@@ -76,19 +69,19 @@ public class FarkleAI {
             model.setHeldDice(updatedHeld);
             view.highlightHeldDice(updatedHeld);
 
-            List<Integer> heldThisTurn = new ArrayList<>();
-            indexInNew = 0;
+            List<Integer> allHeldDice = new ArrayList<>();
             for (int i = 0; i < dice.length; i++) {
-                if (!heldAlready[i]) {
-                    if (indexInNew < diceToHold.length && diceToHold[indexInNew]) {
-                        heldThisTurn.add(dice[i]);
-                    }
-                    indexInNew++;
+                if (updatedHeld[i]) {
+                    allHeldDice.add(dice[i]);
                 }
             }
 
-            int selectedScore = model.calculateScore(heldThisTurn);
-            model.addToCurrentScore(selectedScore);
+            int totalHeldScore = model.calculateScore(allHeldDice);
+            if (model.isAnotherTurn()) {
+                model.addToCurrentScore(totalHeldScore);
+            } else {
+                model.setCurrentScore(totalHeldScore);
+            }
             view.updateCurrentScore(model.getCurrentScore());
 
             if (!model.isAnotherTurn()) {
@@ -98,7 +91,7 @@ public class FarkleAI {
             int remainingDice = getRemainingDiceCount();
             double expectedValue = calculateExpectedValue(remainingDice);
 
-            if (model.allDiceHeld() && model.isHotDice() && !heldThisTurn.isEmpty()) {
+            if (model.allDiceHeld() && model.isHotDice() && !allHeldDice.isEmpty()) {
                 // show popup message
                 JOptionPane.showMessageDialog(view, "AI has hot dice! Rolling again.", "Hot Dice",
                         JOptionPane.INFORMATION_MESSAGE);
@@ -138,27 +131,38 @@ public class FarkleAI {
                 return;
             }
 
-            if (rolls >= 3) {
-                if (model.getCurrentScore() >= THRESHOLD_SCORE_TO_BANK) {
-                    // show popup message
+            if (rolls == 3) {
+                if (model.getPlayerScore(model.getCurrentPlayer()) == 0 && model.getCurrentScore() < 500) {
                     JOptionPane.showMessageDialog(view,
-                            "AI has reached the max rolls and is banking points.", "Max Rolls",
-                            JOptionPane.INFORMATION_MESSAGE);
-                    model.bankPoints();
-                    view.updateScoreDisplay(model.getPlayerScore(model.getCurrentPlayer()), model.getCurrentPlayer());
-                } else {
-                    // show popup message
-                    JOptionPane.showMessageDialog(view,
-                            "AI has reached the max rolls, but doesn't have enough points to bank", "Not Enough Points",
-                            JOptionPane.INFORMATION_MESSAGE);
-                    model.bankPoints();
-                    view.updateScoreDisplay(model.getPlayerScore(model.getCurrentPlayer()), model.getCurrentPlayer());
+                            "AI doesn't have enough points to bank. Ending turn.",
+                            "Not Enough Points", JOptionPane.INFORMATION_MESSAGE);
                     model.setCurrentScore(0);
+                    view.resetForNextTurn();
+                    model.endTurn();
+                    view.updateTurnLabel(model.getCurrentPlayer());
+                    return;
+                } else {
+                    JOptionPane.showMessageDialog(view,
+                            "AI is banking " + model.getCurrentScore() + " points.", "Banking Points",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    model.bankPoints();
+                    // check for winning condition and then display both players' scores
+                    if (model.getPlayerScore(model.getCurrentPlayer()) >= model.getWinningScore()) {
+                        JOptionPane.showMessageDialog(view,
+                                "AI wins with the game!" +
+                                        "\nPlayer 1: " + model.getPlayerScore(0) + " points" +
+                                        "\nPlayer 2: " + model.getPlayerScore(1) + " points",
+                                "Game Over", JOptionPane.INFORMATION_MESSAGE);
+                        System.exit(0);
+                    }
+                    view.updateScoreDisplay(model.getPlayerScore(model.getCurrentPlayer()),
+                            model.getCurrentPlayer());
+                    model.endTurn();
+                    view.resetForNextTurn();
+                    view.updateTurnLabel(model.getCurrentPlayer());
+                    return;
                 }
-                model.endTurn();
-                view.resetForNextTurn();
-                view.updateTurnLabel(model.getCurrentPlayer());
-                return;
+
             }
         }
     }
@@ -194,140 +198,54 @@ public class FarkleAI {
     // Helper to generate all possible rolls of N dice
     private int[][] generateAllRolls(int diceCount) {
         int totalCombos = (int) Math.pow(6, diceCount);
-        int[][] rolls = new int[totalCombos][diceCount];
+        int[][] rollCombo = new int[totalCombos][diceCount];
 
         for (int i = 0; i < totalCombos; i++) {
             int num = i;
             for (int j = 0; j < diceCount; j++) {
-                rolls[i][j] = (num % 6) + 1;
+                rollCombo[i][j] = (num % 6) + 1;
                 num /= 6;
             }
         }
 
-        return rolls;
+        return rollCombo;
     }
 
     // Choose which dice to hold based on score contribution
     private boolean[] chooseScoringDice(List<Integer> diceList) {
         boolean[] hold = new boolean[diceList.size()];
-        int[] counts = new int[7];
-        for (int die : diceList) {
-            counts[die]++;
-        }
+        boolean[] used = new boolean[diceList.size()];
 
-        // Straight (1-6)
-        boolean isStraight = true;
-        for (int i = 1; i <= 6; i++) {
-            if (counts[i] != 1) {
-                isStraight = false;
+        while (true) {
+            int bestScore = 0;
+            List<Integer> bestSubsetIndices = new ArrayList<>();
+
+            int n = diceList.size();
+            for (int mask = 1; mask < (1 << n); mask++) {
+                List<Integer> subset = new ArrayList<>();
+                List<Integer> indices = new ArrayList<>();
+
+                for (int i = 0; i < n; i++) {
+                    if (((mask & (1 << i)) != 0) && !used[i]) {
+                        subset.add(diceList.get(i));
+                        indices.add(i);
+                    }
+                }
+
+                int score = model.calculateScore(subset);
+                if (score > bestScore) {
+                    bestScore = score;
+                    bestSubsetIndices = indices;
+                }
+            }
+
+            if (bestScore == 0) {
                 break;
             }
-        }
-        if (isStraight) {
-            Arrays.fill(hold, true);
-            return hold;
-        }
 
-        // Three pairs
-        int pairCount = 0;
-        for (int i = 1; i <= 6; i++) {
-            if (counts[i] == 2) {
-                pairCount++;
-            }
-        }
-        if (pairCount == 3) {
-            Arrays.fill(hold, true);
-            return hold;
-        }
-
-        // Two triplets
-        List<Integer> triplets = new ArrayList<>();
-        for (int i = 1; i <= 6; i++) {
-            if (counts[i] == 3) {
-                triplets.add(i);
-            }
-        }
-        if (triplets.size() == 2) {
-            for (int triplet : triplets) {
-                int need = 3;
-                for (int i = 0; i < diceList.size(); i++) {
-                    if (diceList.get(i) == triplet && need > 0) {
-                        hold[i] = true;
-                        need--;
-                    }
-                }
-            }
-            return hold;
-        }
-
-        // Four of a kind + a pair
-        for (int i = 1; i <= 6; i++) {
-            if (counts[i] == 4) {
-                for (int j = 1; j <= 6; j++) {
-                    if (i != j && counts[j] == 2) {
-                        Arrays.fill(hold, true);
-                        return hold;
-                    }
-                }
-            }
-        }
-
-        // Six of a kind
-        for (int i = 1; i <= 6; i++) {
-            if (counts[i] == 6) {
-                Arrays.fill(hold, true);
-                return hold;
-            }
-        }
-
-        // Five of a kind
-        for (int i = 1; i <= 6; i++) {
-            if (counts[i] >= 5) {
-                int need = 5;
-                for (int j = 0; j < diceList.size(); j++) {
-                    if (diceList.get(j) == i && need > 0) {
-                        hold[j] = true;
-                        need--;
-                    }
-                }
-                counts[i] -= 5;
-            }
-        }
-
-        // Four of a kind
-        for (int i = 1; i <= 6; i++) {
-            if (counts[i] >= 4) {
-                int need = 4;
-                for (int j = 0; j < diceList.size(); j++) {
-                    if (diceList.get(j) == i && need > 0) {
-                        hold[j] = true;
-                        need--;
-                    }
-                }
-                counts[i] -= 4;
-            }
-        }
-
-        // Three of a kind
-        for (int i = 1; i <= 6; i++) {
-            if (counts[i] >= 3) {
-                int need = 3;
-                for (int j = 0; j < diceList.size(); j++) {
-                    if (diceList.get(j) == i && need > 0) {
-                        hold[j] = true;
-                        need--;
-                    }
-                }
-                counts[i] -= 3;
-            }
-        }
-
-        // Remaining 1s and 5s
-        for (int i = 0; i < diceList.size(); i++) {
-            int die = diceList.get(i);
-            if ((die == 1 && counts[1] > 0) || (die == 5 && counts[5] > 0)) {
-                hold[i] = true;
-                counts[die]--;
+            for (int idx : bestSubsetIndices) {
+                hold[idx] = true;
+                used[idx] = true;
             }
         }
 
